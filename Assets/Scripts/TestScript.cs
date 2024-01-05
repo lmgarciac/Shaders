ed16fdcfc59e82637d1be0e8fd9a7ef7f5477f10
lmgarciac <30421890+lmgarciac@users.noreply.github.com>
2024-01-05 13:17:02 -0300

    Normal Mapping
    Normal Mapping

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestTestest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Imma Test Script");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
